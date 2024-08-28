b639f78ed75f5308c95570491434e08b5f2717bf
Divyaa01 <divya73908@gmail.com>
2024-08-28 14:35:16 +0530

    basic list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [inputValue, setInputValue] = useState("");
+const [tasks, setTasks] = useState([]);
+
+const addTask = () =>{
+  if( inputValue.trim()!== ""){
+  setTasks([...tasks , inputValue]);
+  setInputValue("");
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To-do List</h2>
+      <input 
+      type ="text"
+      value= {inputValue}
+      onChange = {(e) => setInputValue(e.target.value)}
+      placeholder ="add here"/>
+
+      <button onClick ={addTask}> Add tasks</button>
+
+      {
+        <ul>
+          {
+            tasks.map((task) =>
+            (
+              <li>{task}</li>
+            ))
+          }
+        </ul>
+      }
     </div>
   );
 }
